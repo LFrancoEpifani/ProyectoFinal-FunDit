93d6cf83ea6254fd472de0c7911c6429a6e4b56c
LucianoEsplai <lfrancoepifani@gmail.com>
2023-11-14 15:14:58 +0100

    Las cards ya se dan vuelta perfectamente y muestran el contenido adecuado

--- fundit/src/components/Card/Card.jsx
+++ fundit/src/components/Card/Card.jsx
@@ -1,42 +1,98 @@
-import React from 'react';
-import {Icon} from '@iconify/react'
-
-import {client} from '../../../../fundit-backend/.sanity/lib/client'
+import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
+import { client } from "../../../../fundit-backend/.sanity/lib/client";
 
 export async function getFlyers() {
   const query = `*[_type == "flyer"]{
+    _id,
     title,
     description,
     price,
     location,
     schedule,
-    "imageUrl": image.asset->url
+    "imageUrl": image.asset->url,
+    socialMedia[]{
+      platform,
+      url
+    }
   }`;
 
-  const flyers = await client.fetch(query, {cache: "no-store" })
+  const flyers = await client.fetch(query, { cache: "no-store" });
   return flyers;
 }
 
 export default function Card() {
-  const [flyers, setFlyers] = React.useState([]);
+  const [flyers, setFlyers] = useState([]);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     getFlyers().then(flyersData => {
-      setFlyers(flyersData);
+      const flyersWithDefaults = flyersData.map(flyer => ({
+        ...flyer,
+        socialMedia: flyer.socialMedia || [] // Provide an empty array as a default value
+      }));
+      setFlyers(flyersWithDefaults);
     });
   }, []);
 
+  const openSocialMedia = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const flipHandle = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className='grid grid-cols-4 flipped-card-container'>
-      {flyers.map(flyer => (
-        <div key={flyer._id}>
-            <h1>{flyer.title}</h1>
-          <img className='myImage m-4' src={flyer.imageUrl} alt={flyer.title} />
-          <p>{flyer.description}</p>
+    <div className="flex flex-wrap" >
+      {flyers.map((flyer) => (
+        <div key={flyer._id} className="card">
+          <Icon onClick={flipHandle} className="flip-icon" icon="mi:switch" />
+          <div className={`card-inner ${isFlipped ? "is-flipped" : ""}`}>
+            <div
+              className="card-front"
+              style={{ backgroundImage: `url(${flyer.imageUrl})` }}
+            >
+              
+            </div>
+
+
+            <div
+              className="card-back text-white"
+              style={{ backgroundImage: `url(${flyer.imageUrl})` }}
+            >
+              <div className="m-12 text-xl font-bold uppercase text-center">
+                <h3 className="">{flyer.title}</h3>
+              </div>
+              <div className="descripction m-8 text-center text-md font-light">
+                <p>{flyer.description}</p>
+              </div>
+              <div className="flex justify-between m-4 text-lg">
+                <p>{flyer.schedule}hs</p>
+                <p className="capitalize"  style={{ color: flyer.price === 'gratis' ? 'text-green-500 font-bold' : 'inherit' }}>{flyer.price}</p>
+              </div>
+              <div className="text-center gap-2 flex justify-center items-center m-4">
+              <Icon className="text-xl" icon="gis:location-poi" color="red" />
+                <p className="location text-lg -tracking-tight">{flyer.location}</p>
+              </div>
+              <div className="flex justify-around items-center m-12 text-xl">
+                <button>
+                  <Icon icon="ic:baseline-facebook" color="white" />
+                </button>
+                <button>
+                  <Icon icon="mdi:twitter" color="white" />
+                </button>
+                <button>
+                  <Icon icon="mdi:instagram" color="white" />
+                </button>
+                <button>
+                  <Icon icon="ic:baseline-whatsapp" color="white" />
+                </button>
+            </div>
+          </div>
         </div>
-        
+        </div>
       ))}
     </div>
   );
 }
-
--- fundit/src/components/Header/Header.jsx
+++ fundit/src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card'
 
-
 export default function Header() {
   return (
     <div className='h-screen w-full'>
