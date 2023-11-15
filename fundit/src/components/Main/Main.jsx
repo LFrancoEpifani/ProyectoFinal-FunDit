889331919c943ca014aa99f98be3031b66a14936
LucianoEsplai <lfrancoepifani@gmail.com>
2023-11-15 19:39:47 +0100

    Taking off Main

--- fundit/src/components/Main/Main.jsx
+++ fundit/src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ export default function Main() {
   return (
     <div className=''>
     <div className='shadow-lg flex justify-center items-center'>
-        <img className='w-full h-screen object-cover object-center' src={FunDit} alt="" />
+        
     </div>
     <div className=''>
         <Card />
