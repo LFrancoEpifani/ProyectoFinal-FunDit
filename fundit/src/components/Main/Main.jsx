34b2955c13abffaba255ec86a90487dcae2add36
LucianoEsplai <lfrancoepifani@gmail.com>
2023-11-15 19:25:10 +0100

    Vercel

--- fundit/src/components/Main/Main.jsx
+++ fundit/src/components/Main/Main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import FunDit1 from '../../assets/funditlogo2.png'
-import FunDit2 from '../../assets/funditlogo3.png'
+import FunDit from '../../assets/funditlogo2.png'
 import Card from "../Card/Card";
 
 export default function Main() {
   return (
     <div className=''>
     <div className='shadow-lg flex justify-center items-center'>
-        <img className='w-3/6 h-screen object-cover object-left' src={FunDit2} alt="" />
-        <img className='w-2/6 h-screen object-cover object-center' src={FunDit1} alt="" />
-        <img className='w-3/6 h-screen object-cover object-right' src={FunDit2} alt="" />
+        <img className='w-full h-screen object-cover object-center' src={FunDit} alt="" />
     </div>
     <div className=''>
         <Card />
