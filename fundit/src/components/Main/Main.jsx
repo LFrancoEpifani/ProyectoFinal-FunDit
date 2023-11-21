ad75a9f9531a0bc78fbfe41d1d8f9041f4d73712
LucianoEsplai <lfrancoepifani@gmail.com>
2023-11-21 19:42:49 +0100

    Updating images

--- fundit/src/components/Main/Main.jsx
+++ fundit/src/components/Main/Main.jsx
@@ -9,13 +9,13 @@ export default function Main() {
   <div>
     <NavBar/>
     <div className='bienvenida'>
-      <img className='w-full h-full' src={Fondo} alt="" />
-      <h2 className='text-center text-2xl '>
+    <h2 className='text-center text-2xl m-4'>
         Bienvenido a <span className='font-bold fundit'>FUNDIT</span>
       </h2>
+      <img className='w-full h-full' src={Fondo} alt="" />
     </div>
     <div className='flex justify-center items-center'>
-    <button className='cartelera text-black text-center rounded-full w-40 h-8 font-semibold tracking-wider border border-black shadow-md'>
+    <button className='text-black text-center rounded-full w-40 h-8 font-semibold tracking-wider border border-black shadow-md'>
       CARTELERA
     </button> 
   </div>
